a2f987a14c56ca2aac393bf87f21ebbbb663467c
nguyenduclongwin <nguyenduclongwin@gmail.com>
2020-02-18 17:54:49 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,28 @@
+require('./bootstrap');
+
+window.Vue = require('vue');
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component(
+    'passport-clients',
+    require('./components/passport/Clients.vue').default
+);
+
+Vue.component(
+    'passport-authorized-clients',
+    require('./components/passport/AuthorizedClients.vue').default
+);
+
+Vue.component(
+    'passport-personal-access-tokens',
+    require('./components/passport/PersonalAccessTokens.vue').default
+);
+Vue.component('profile-component', require('./components/ProfileComponent.vue').default);
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+});
